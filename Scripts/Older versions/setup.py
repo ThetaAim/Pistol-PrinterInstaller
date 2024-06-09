fd985c3e0bf4f402ce254c0dad6ccb72ed585ecd
Oren Ohayon <mailorenn@gmail.com>
2024-06-09 13:48:46 +0300

    how to install

--- Scripts/Older versions/setup.py
+++ Scripts/Older versions/setup.py
@@ -40,4 +40,15 @@ setup(
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
+    install_requires=[
+        'altgraph==0.17.4',
+        'macholib==1.16.3',
+        'modulegraph==0.19.6',
+        'packaging==24.0',
+        'pipdeptree==2.22.0',
+        'platformdirs==4.2.2',
+        'py2app==0.28.8',
+        'setuptools==70.0.0',
+        'typing_extensions==4.12.1'
+    ],
 )
