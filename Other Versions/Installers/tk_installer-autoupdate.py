b8b98fb5b35f91b2d9e22333fc092bbbd0ba3e8c
Oren Ohayon <mailorenn@gmail.com>
2024-07-02 15:33:01 +0300

    file structure changes

---
+++ Other Versions/Installers/tk_installer-autoupdate.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+import tkinter as tk
+import subprocess
+
+log_file = "/tmp/Pistol_log.txt"
+
+def check_file_exists(pkg_path):
+    """Check if the given file path exists."""
+    return os.path.isfile(pkg_path)
+
+def install_pkgs(pkgs, text_widget, event):
+    """Install the specified packages using AppleScript for administrative privileges."""
+    applescript_commands = ''
+    tmp_paths = []
+    for pkg_path, file_name in pkgs:
+        if not check_file_exists(pkg_path):
+            text_widget.insert(tk.END, f"Package path {pkg_path} does not exist.\n")
+            continue
+
+        tmp_pkg_path = f'/tmp/{file_name}.pkg'
+        tmp_paths.append(tmp_pkg_path)
+        try:
+            shutil.copy(pkg_path, tmp_pkg_path)
+        except Exception as e:
+            text_widget.insert(tk.END, f"Failed to copy {pkg_path} to {tmp_pkg_path}: {e}\n")
+            continue
+
+        # Collects Commands in lines for AppleScript to run
+        applescript_commands += (f'do shell script "bash -c \\"/usr/sbin/installer -verbose -pkg {tmp_pkg_path} '
+                                 f'-target / >> {log_file} 2>&1\\"" with administrator privileges\n')
+
+    text_widget.insert(tk.END, f"\nDone Coping files.\n")
+    text_widget.insert(tk.END, "\nWaiting for authentication...\n")
+    text_widget.insert(tk.END, "\nThis Might take up to 10 minutes, Please wait\n")
+
+    full_applescript = f"osascript -e '{applescript_commands}'"
+
+    # Run the command using os.system
+    result = os.system(full_applescript)
+
+    if result == 0:
+        text_widget.insert(tk.END, f"Installed All packages successfully.\n")
+    else:
+        text_widget.insert(tk.END, f"Installation failed.\n")
+        exit()
+
+    # Remove temp file(s)
+    for tmp in tmp_paths:
+        if os.path.exists(tmp):
+            os.remove(tmp)
+            text_widget.insert(tk.END, f"Cleaned up temp file.\n")
+
+    # Signal Event Done, all packages have been installed
+    event.set()
---
+++ Other Versions/Presets/2.7/Copy_Prst_original.py
@@ -0,0 +1,64 @@
+import os
+import shutil
+import pwd
+
+# Get the current username
+# username = os.popen('whoami').read().strip()
+username = os.getenv('USER')
+
+
+def copy_files(source_folder, destination_folder):
+
+    list_of_files_with_full_paths = []
+    files = os.listdir(source_folder)
+
+    if not files:
+        print("No files found in the source folder.")
+        return
+
+    for file in files:
+        if os.path.isfile(os.path.join(source_folder, file)):
+            list_of_files_with_full_paths.append(os.path.join(source_folder, file))
+        else:
+            print(f"The file {file} is missing or not a regular file. Unable to copy.")
+
+    try:
+        # Create a temporary folder in /tmp
+        temp_folder = "/tmp/presets"
+        if not os.path.exists(temp_folder):
+            os.makedirs(temp_folder)
+
+        # Copy valid files from the source folder to the temporary folder
+        for source_file in list_of_files_with_full_paths:
+            shutil.copy(source_file, temp_folder)
+            print(f"Copied {source_file} to {temp_folder}")
+
+        # Change ownership of the copied files to the current user
+        files_in_tmp_folder = os.listdir(temp_folder)
+        if not files_in_tmp_folder:
+            print('No Files in temp')
+            return
+        
+        uid = pwd.getpwnam(username).pw_uid
+        gid = pwd.getpwnam(username).pw_gid
+
+        for tmp_file in files_in_tmp_folder:
+            full_path_tmp_file = os.path.join(temp_folder, tmp_file)
+            os.chown(full_path_tmp_file, uid, gid)
+            print(f"Changed ownership for {tmp_file} to user {username}.")
+
+        # Move files from the temporary folder to the destination folder
+        for tmp_file in files_in_tmp_folder:
+            destination_file = os.path.join(destination_folder, tmp_file)
+            shutil.move(os.path.join(temp_folder, tmp_file), destination_file)
+            print(f"Moved and overwritten {tmp_file} to {destination_file}")
+
+        print("All files moved successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Clean up the temporary folder
+        try:
+            shutil.rmtree(temp_folder, ignore_errors=True)
+        except Exception as e:
+            print(f"Failed to delete temporary folder {temp_folder}: {e}")
